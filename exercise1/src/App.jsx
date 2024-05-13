7e438030ff84fa2a4e1383492dc3f9cbd81cf590
vino-brutor <viktorbruno2006@gmail.com>
2024-05-13 17:43:48 -0300

    first commit

---
+++ exercise1/src/App.jsx
@@ -0,0 +1,40 @@
+export default function App(){
+  return(
+    <div className="container-fluid">
+      <div className="container">
+      <img className="pb-4" src="https://onebitcode.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fea7d49f7-85ff-4dc5-96bf-7101fd3e1565%2Freact.png?table=block&id=8bc24587-6336-4b7b-9fe0-1a5d63bccbc8&spaceId=6e5271d8-2f68-42f5-aa75-5978bbff47fa&width=270&userId=&cache=v2" alt="React-logo" />
+        <h2>React</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam nesciunt blanditiis, vitae quas ut facilis minus non omnis adipisci possimus labore reiciendis? Debitis nobis nesciunt ducimus quaerat consequuntur nisi expedita numquam aliquid laudantium mollitia, ad at soluta unde, voluptas tempore.</p>
+        <div className="row">
+          <button className="btn btn-primary col p-2 m-2">Saber mais sobre react</button>
+          <button className="btn btn-primary col p-2 m-2">Página da API</button>
+        </div>
+        <hr />
+        <div className="container">
+          <h2>Crie interfaces interativas</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, asperiores! Nihil, voluptates error hic molestias ex aliquam nisi odit quibusdam voluptatum. Ad doloremque ipsam adipisci aliquam neque temporibus repellendus placeat tempora. Obcaecati minus exercitationem maxime et non eos debitis sapiente.</p>
+        </div>
+        <hr />
+        
+        <div className="container">
+          <h2>Crie componentes com códigos de marcação</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, asperiores! Nihil, voluptates error hic molestias ex aliquam nisi odit quibusdam voluptatum. Ad doloremque ipsam adipisci aliquam neque temporibus repellendus placeat tempora. Obcaecati minus exercitationem maxime et non eos debitis sapiente.</p>
+        </div>
+        <hr />
+
+        <div className="container">
+          <h2>Próximos passos</h2>
+          <ul>
+            <li>Aprender a usar o jsx de forma dinamica</li>
+            <li>Masterizar os fundamentos deo react</li>
+            <li>criação e estilização de componentes</li>
+            <li>uso dde pops e childrens</li>
+            <li>misturar com eventos javascript</li>
+          </ul>
+        </div>
+        
+      </div>
+    </div>
+    
+  )
+} 
